e8203ca3addb5644c71276baabfdcff38642f4c0
unknown <orlando.aguilar@digipen.edu>
2016-01-28 19:32:40 -0800

    joining Signal and Slot headers, adding comments to Signal.cpp and cleanning visual studio files

--- Signal.h
+++ Signal.h
@@ -1,5 +1,26 @@
 #pragma once
+
+/*
+Copyright 2015 Orlando Aguilar Vivanco
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
 #include "Callable.h"
+#include <list>
+#include "Delegate.h"
+#include "PointerFunc.h"
 
 namespace siglot{
 	template<typename... args>
@@ -38,5 +59,58 @@ namespace siglot{
 			m_slots.remove(slot);
 		}
 		std::list< tSlot*> m_slots;
+	};
+
+	template<typename... args>
+	class Slot{
+		typedef Callable<void, args...> tCallable;
+		typedef Signal<args...> tSignal;
+		friend class tSignal;
+	public:
+		void operator()(args... ar){
+			for (auto callable : m_callable){
+				(*callable)(ar...);
+			}
+		}
+
+		template<typename tClass>
+		void Attach(tClass * _oClass, void(tClass::* tMethod)(args...)){
+			m_callable.push_back(new Delegate<tClass, void, args...>(_oClass, tMethod));
+		}
+
+		void Attach(void(*tPFunc)(args...)){
+			m_callable.push_back(new PointerFunc<void, args...>(tPFunc));
+		}
+
+		template<typename tClass>
+		void Detach(tClass * _oClass, void(tClass::* tMethod)(args...)){
+			Delegate<tClass, void, args...> deleg(_oClass, tMethod);
+			m_callable.remove_if([&deleg](tCallable* c){return (c->Equal(deleg) ? delete c, true : false); });
+		}
+
+		void Detach(void(*tPFunc)(args...)){
+			PointerFunc<void, args...> pF(tPFunc);
+			m_callable.remove_if([&pF](tCallable* c){return (c->Equal(pF) ? delete c, true : false); });
+		}
+
+		~Slot(){
+			for (tSignal* signal : m_signals){
+				signal->RemoveSlot(this);
+			}
+			for (tCallable* callable : m_callable){
+				delete callable;
+			}
+		}
+	private:
+
+		void AppendSignal(tSignal* signal){
+			m_signals.push_back(signal);
+		}
+		void RemoveSignal(tSignal* signal){
+			m_signals.remove(signal);
+		}
+
+		std::list< tCallable*> m_callable;
+		std::list< tSignal*> m_signals;
 	};
 }
--- Signals.cpp
+++ Signals.cpp
@@ -1,15 +1,35 @@
-// Signals.cpp : Defines the entry point for the console application.
+/*
+Copyright 2015 Orlando Aguilar Vivanco
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+// Example of use.
 //
 
 #include <iostream>
 #include <list>
 #include "Signal.h"
-#include "Slot.h"
 #include "Delegate.h"
 #include "PointerFunc.h"
 
 using namespace siglot;
 using namespace std;
+
+void empty(){
+	cout << "Empty" << endl;
+}
 
 void myFloat(int a, float b){
 	cout << a<<b <<"my float"<< endl;
@@ -32,33 +52,39 @@ public:
 
 int main(int argc, char* argv[])
 {
-	Slot<int, float> slot;
+	Classe myClass;
 	
-	Classe clase;
+	Slot<int, float> slot;		//declaring a slot that holds functions and functors which receive 2 parameters, an int and a float
+	Signal<int, float> signal;	//declares a signal compatible with the previous slot
+	
 		
-	Signal<int, float> signal;
+	slot.Attach(&myClass, &Classe::miMethod);	//adds to the slot a member method of myClass
+	slot.Attach(&myFloat);						//adds to the 
+	slot.Attach(&Classe::StaticMethod);			//adds a static method to the slot
 
-	slot.Attach(&clase, &Classe::miMethod);
+	signal.Connect(&slot);						//connects the signal and the slot, so that firing the signal will also fire all the elements connected to the slot
+	signal(2, 3);								//fires the signal, which will fire the three elements connected to the slot
+
+	slot.Detach(&myFloat);						//detaches the function from the slot
+	signal(2, 4);								//now, firing the signal will only fire two elements of the slot
+
+	slot.Detach(&myClass, &Classe::miMethod);	//detaches the member of myClass
+	signal(2, 5);								//fires the only one element of the slot
+
+	slot.Detach(&myClass, &Classe::miMethod2);	//tryies to detach an element that is not present in the slot. Nothing happens
+	signal(2, 6);								
 		
-	slot.Attach(&myFloat);
+	slot.Detach(&Classe::StaticMethod);			//detaches the last element of the signal
+	signal(2, 7);								//slot empty, nothing is fired
 
-	slot.Attach(&Classe::StaticMethod);
+	//signal and slot for members with empty parameter list
+	Signal<> empParamSignal;
+	Slot<> empParamSlot;
 
-	signal.Connect(&slot);
-	signal(2, 3);
+	empParamSlot.Attach(&empty);
 
-	slot.Detach(&myFloat);
-	signal(2, 4);
-
-	slot.Detach(&clase, &Classe::miMethod);
-	signal(2, 5);
-
-	slot.Detach(&clase, &Classe::miMethod2);
-	signal(2, 6);
-		
-	slot.Detach(&Classe::StaticMethod);
-	signal(2, 7);
-
+	empParamSignal.Connect(&empParamSlot);
+	empParamSignal();
 	return 0;
 }
 
